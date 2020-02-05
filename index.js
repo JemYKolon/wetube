b6f624faa4d23a6ccf9869210771764b1698ba64
herroa <32617049+herroa@users.noreply.github.com>
2020-02-05 16:20:37 +0900

    ExpressJS
    2.0-2.3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hi");
+
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+ 
+app.listen(PORT, handleListening)
