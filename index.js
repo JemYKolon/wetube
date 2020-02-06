82d61479d651479c68909e78b512c9671629fea5
herroa <32617049+herroa@users.noreply.github.com>
2020-02-06 16:39:46 +0900

    2.4-2.7
    ExpressJS

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 
-const express = require('express');
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieparser from "cookie-parser";
+import bodyparser from "body-parser";
 const app = express();
 
 const PORT = 4000;
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
- 
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieparser());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening)
