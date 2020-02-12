9a29e3703f0452fb5600c254e6cabddc1c23c86a
herroa <herro2546@gmail.com>
2020-02-12 19:32:06 +0900

    2.24-3.1
    ExpressJS 24-25
    MongoDB 0-1

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,8 +18,17 @@ export const postJoin = (req, res) => {
   }
 };
 
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log in" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+  // To Do: Process Log out
+  res.redirect(routes.home);
+};
+
 export const users = (req, res) => res.render("users");
 export const userDetail = (req, res) => res.render("userDetail");
 export const editProfile = (req, res) => res.render("editProfile");
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -2,10 +2,11 @@ import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
 import {
-  login,
-  logout,
   getJoin,
-  postJoin
+  getLogin,
+  logout,
+  postJoin,
+  postLogin
 } from "../controllers/userController";
 
 const globalRouter = express.Router();
@@ -15,7 +16,9 @@ globalRouter.get(routes.home, home);
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
-globalRouter.get(routes.login, login);
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
 
