a3dcbd2cd6eeb1c6e9d66be7bfa4b13a2c666201
herroa <herro2546@gmail.com>
2020-02-13 21:47:10 +0900

    3.2-3.9
    MongoDB

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
+mongoose.set("useUnifiedTopology", true);
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect("mongodb://localhost:27017/wetube", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -8,7 +11,7 @@ mongoose.connect("mongodb://localhost:27017/wetube", {
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅  Connected to DB");
-const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+const handleError = error => console.log(`❌  Error on DB Connection:${error}`);
 
 db.once("open", handleOpen);
 db.on("error", handleError);
